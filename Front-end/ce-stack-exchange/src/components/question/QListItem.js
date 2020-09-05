d59d67b5efd1c3e6ffc00af327ca1fffadd99ac4
Mahta Fetrat <77fetrat@gmail.com>
2020-09-06 00:29:36 +0430

    QListItem functionalize

--- Front-end/ce-stack-exchange/src/components/question/QListItem.js
+++ Front-end/ce-stack-exchange/src/components/question/QListItem.js
@@ -2,10 +2,82 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography, Chip, Grid } from "@material-ui/core";
 
+//sample tag list
+const tags = [
+  {
+    label: "جاوا",
+    href: "#java",
+  },
+  {
+    label: "شیء گرایی",
+    href: "#object-orientated",
+  },
+  {
+    label: "ای پی",
+    href: "#AP",
+  },
+];
+
 const useStyles = makeStyles((theme) => ({}));
 
 export default function QListItem() {
   const classes = useStyles();
+
+  const QCardInfo = (color, bgColor, label, value) => (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-evenly"
+      flexDirection="column"
+      borderColor={color}
+      border={2}
+      borderRadius={3}
+      width={70}
+      height={70}
+    >
+      <body1 style={{ color: color }}>{value}</body1>
+      <body1 style={{ color: color }}>{label}</body1>
+    </Box>
+  );
+
+  const Tag = (label, href) => (
+    <Chip clickable label={label} component="a" href={href} size="small" />
+  );
+
+  const QInfo = (
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-evenly"
+      width={"20%"}
+      mr={2}
+    >
+      {QCardInfo("#919993", "transparent", "vote", 0)}
+      {QCardInfo("#32a852", "transparent", "answer", 6)}
+      {QCardInfo("#919993", "transparent", "views", 23)}
+    </Box>
+  );
+
+  const QContext = (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-evenly"
+      style={{ width: "80%" }}
+    >
+      <Typography>"چرا جاوا امکان ارث بری از چند کلاس را نمیدهد؟"</Typography>
+      <Grid container spacing={2} style={{ width: "100%" }}>
+        <Grid item>{tags.map((tag) => Tag(tag.label, tag.href))}</Grid>
+        <Grid item style={{ flexGrow: 1 }}>
+          <div></div>
+        </Grid>
+        <Grid item>
+          <Typography variant="overline">علی علوی </Typography>
+          <Typography variant="overline">_ پرسیده شده 2 ساعت پیش</Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 
   return (
     <div style={{ margin: 100 }}>
@@ -17,101 +89,8 @@ export default function QListItem() {
         justifyContent="flex-start"
         height="80"
       >
-        <Box
-          display="flex"
-          flexDirection="row"
-          justifyContent="space-evenly"
-          width={"20%"}
-          mr={2}
-        >
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-evenly"
-            flexDirection="column"
-            borderColor="#919993"
-            border={2}
-            borderRadius={3}
-            width={70}
-            height={70}
-          >
-            <body1 style={{ color: "#919993" }}>0</body1>
-            <body1 style={{ color: "#919993" }}>votes</body1>
-          </Box>
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-evenly"
-            flexDirection="column"
-            borderColor="#32a852"
-            border={2}
-            borderRadius={3}
-            width={70}
-            height={70}
-          >
-            <body1 style={{ color: "#32a852" }}>6</body1>
-            <body1 style={{ color: "#32a852" }}>answers</body1>
-          </Box>
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-evenly"
-            flexDirection="column"
-            borderColor="#919993"
-            border={2}
-            borderRadius={3}
-            width={70}
-            height={70}
-          >
-            <body1 style={{ color: "#919993" }}>23</body1>
-            <body1 style={{ color: "#919993" }}>views</body1>
-          </Box>
-        </Box>
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-evenly"
-          style={{ width: "80%" }}
-        >
-          <Typography>
-            "چرا جاوا امکان ارث بری از چند کلاس را نمیدهد؟"
-          </Typography>
-
-          <Grid container spacing={2} style={{ width: "100%" }}>
-            <Grid item>
-              <Chip
-                clickable
-                label="ای پی"
-                component="a"
-                href="#AP"
-                size="small"
-              />
-              <Chip
-                clickable
-                label="جاوا"
-                component="a"
-                href="#AP"
-                size="small"
-              />
-              <Chip
-                clickable
-                label="شیء گرایی"
-                component="a"
-                href="#AP"
-                size="small"
-              />
-            </Grid>
-            <Grid item style={{ flexGrow: 1 }}>
-              <div></div>
-            </Grid>
-            <Grid item>
-              <Typography variant="overline">علی علوی </Typography>
-              <Typography variant="overline">
-                _ پرسیده شده 2 ساعت پیش
-              </Typography>
-            </Grid>
-          </Grid>
-        </Box>
+        <QInfo/>
+        <QContext/>
       </Box>
     </div>
   );
