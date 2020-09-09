563246316043524fb2baa679d2a185c670ce964a
Mahta Fetrat <77fetrat@gmail.com>
2020-09-10 00:13:43 +0430

    edit password form validation

--- Front-end/ce-stack-exchange/src/components/DashboardComponents/profile/EditPassword.js
+++ Front-end/ce-stack-exchange/src/components/DashboardComponents/profile/EditPassword.js
@@ -9,24 +9,25 @@ import {
   Divider,
   Box,
   Button,
+  FormHelperText,
 } from "@material-ui/core";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 const useStyles = makeStyles((theme) => ({
-  passwordField : {
-    width : '50%',
-    [theme.breakpoints.down('sm')] : {
-      width : '100%'
-    }
+  passwordField: {
+    width: "50%",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
   },
-  container : {
-    padding : 40,
-    [theme.breakpoints.down('sm')] : {
-      padding : 15
-    }
-  }
-}))
+  container: {
+    padding: 40,
+    [theme.breakpoints.down("sm")]: {
+      padding: 15,
+    },
+  },
+}));
 
 const fields = [
   {
@@ -53,16 +54,52 @@ const fields = [
 ];
 
 export default function InputAdornments() {
-const classes = useStyles();
+  const classes = useStyles();
 
   const [passFields, setPassFields] = React.useState({
     visible0: false,
     value0: "",
+    helper0: "",
+    error0: false,
+
     visible1: false,
-    value1visible1: "",
+    value1: "",
+    error1: false,
+
     visible2: false,
-    value2visible2: "",
+    value2: "",
+    helper2: "",
+    error2: false,
   });
+
+  const isPasswordCorrect = () => {
+    //TODO
+    return true;
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (!isPasswordCorrect) {
+      setPassFields({
+        ...passFields,
+        error0: true,
+        helper0: "رمز عبور صحیح نیست",
+      });
+      return;
+    }
+
+    if (passFields.value1 !== passFields.value2) {
+      setPassFields({
+        ...passFields,
+        error2: true,
+        error1: true,
+        helper2: "رمز عبور و تکرار آن مطابقت ندارند",
+      });
+      return;
+    }
+
+    //TODO
+  };
 
   const handleChange = (fieldName) => (event) => {
     setPassFields({
@@ -80,9 +117,14 @@ const classes = useStyles();
 
   const passField = (index, label, labelWidth, bottomSeparator) => (
     <>
-      <FormControl variant="outlined" className={classes.passwordField}>
+      <FormControl
+        variant="outlined"
+        className={classes.passwordField}
+        error={passFields["error" + index]}
+      >
         <InputLabel>{label}</InputLabel>
         <OutlinedInput
+          required
           value={passFields["value" + index]}
           onChange={handleChange("value" + index)}
           type={passFields["visible" + index] ? "text" : "password"}
@@ -100,6 +142,7 @@ const classes = useStyles();
           }
           labelWidth={labelWidth}
         ></OutlinedInput>
+        <FormHelperText>{passFields["helper" + index]}</FormHelperText>
       </FormControl>
       {bottomSeparator}
     </>
@@ -107,17 +150,31 @@ const classes = useStyles();
 
   return (
     <Paper style={{ marginBottom: 60 }}>
-      <Box display="flex" flexDirection="column" alignItems="flex-start" className={classes.container}>
-        {fields.map((field, index) =>
-          passField(index, field.label, field.labelWidth, field.bottomSeparator)
-        )}
-        <br />
-        <br />
-        <Divider style={{ alignSelf: "stretch" }} />
-        <br />
-        <br />
-        <Button style={{ alignSelf: "flex-end", fontSize: 18 }}>تایید</Button>
-      </Box>
+      <form onSubmit={submitForm} noValidate>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="flex-start"
+          className={classes.container}
+        >
+          {fields.map((field, index) =>
+            passField(
+              index,
+              field.label,
+              field.labelWidth,
+              field.bottomSeparator
+            )
+          )}
+          <br />
+          <br />
+          <Divider style={{ alignSelf: "stretch" }} />
+          <br />
+          <br />
+          <Button type="submit" style={{ alignSelf: "flex-end", fontSize: 18 }}>
+            تایید
+          </Button>
+        </Box>
+      </form>
     </Paper>
   );
 }
