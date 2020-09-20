0f8f778bc95f9ccf3570b4b76381cf7fb863cd44
Dana <danaafa1380@gmail.com>
2020-09-20 23:09:06 +0430

    Community.go database implemented

--- Back-end/src/Model/Community.go
+++ Back-end/src/Model/Community.go
@@ -1,11 +1,16 @@
 package Model
 
+import (
+	"database/sql"
+	"errors"
+)
+
 type Community struct {
-	Id       string `json:"id" yaml:"-"`
-	Name     string `json:"name" yaml:"name"`
-	InfoText string `json:"info_text" yaml:"-"`
-	Deleted  bool   `json:"deleted" yaml:"-"`
-	Date     string
+	Id          string `json:"id" yaml:"-"`
+	Name        string `json:"name" yaml:"name"`
+	Description string `json:"info_text" yaml:"-"`
+	Deleted     bool   `json:"deleted" yaml:"-"`
+	Date        string
 }
 
 func initCommunityTable() error {
@@ -21,22 +26,73 @@ func initCommunityTable() error {
 	return err
 }
 
-func GetAllCommunities(deleted ...bool) ([]Community, error) {
-	return nil, nil
-}
-
 func GetCommunityById(communityId string, deleted ...bool) (*Community, error) {
-	return nil, nil
+	// checking if the deleted config is specified
+	var isDeleted = false
+	if len(deleted) != 0 {
+		isDeleted = deleted[0]
+	}
+
+	// retrieving and processing the result
+	var community = Community{}
+	r := db.QueryRow(`SELECT * FROM communities WHERE community_id = $1 AND deleted = $2`, communityId, isDeleted)
+	err := r.Scan(&community.Id, &community.Name, &community.Description, &community.Date, &community.Deleted)
+
+	switch err {
+	case sql.ErrNoRows:
+		return nil, errors.New("there is no such community")
+	case nil:
+		return &community, nil
+	default:
+		return nil, err
+	}
 }
 
 func GetCommunityByName(communityName string, deleted ...bool) (*Community, error) {
-	return nil, nil
+	// checking if the deleted config is specified
+	var isDeleted = false
+	if len(deleted) != 0 {
+		isDeleted = deleted[0]
+	}
+
+	// retrieving and processing the result
+	var community = Community{}
+	r := db.QueryRow(`SELECT * FROM communities WHERE community_name = $1 AND deleted = $2`, communityName, isDeleted)
+	err := r.Scan(&community.Id, &community.Name, &community.Description, &community.Date, &community.Deleted)
+
+	switch err {
+	case sql.ErrNoRows:
+		return nil, errors.New("there is no such community")
+	case nil:
+		return &community, nil
+	default:
+		return nil, err
+	}
 }
 
-func NewCommunity(name string, infoText string) (*Community, error) {
-	return nil, nil
+func NewCommunity(name string, description string) (*Community, error) {
+	// add new community
+	r := db.QueryRow(`INSERT INTO communities (
+                     community_name, description)
+					  values ($1, $2) returning *;`,
+		name, description)
+
+	// retrieves the created community and returns the result
+	var newCommunity = Community{}
+	err := r.Scan(&newCommunity.Id, &newCommunity.Name, &newCommunity.Description, &newCommunity.Deleted, &newCommunity.Date)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &newCommunity, nil
 }
 
-func (Community *Community) DeleteCommunity() error {
-	return nil
+func (community *Community) DeleteCommunity() error {
+	r := db.QueryRow(`UPDATE communities 
+							SET is_deleted = TRUE
+							WHERE community_id = $1
+							RETURNING is_deleted			
+	`, community.Id)
+	return r.Scan(&community.Deleted)
 }
