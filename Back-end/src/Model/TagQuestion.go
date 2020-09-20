037d7cb11a7642392a167d9498732783b77c537b
Dana <danaafa1380@gmail.com>
2020-09-21 00:38:25 +0330

    minor bug fix

--- Back-end/src/Model/TagQuestion.go
+++ Back-end/src/Model/TagQuestion.go
@@ -20,6 +20,47 @@ func initTagQuestionTable() error {
 	return err
 }
 
-func (question Question) GetQuestionTags() ([]string, error) {
-	return nil, nil
+func (question *Question) AddTags(tagArr []string) error {
+	// if no tag to add then every thing is fine
+	if tagArr == nil || len(tagArr) == 0 {
+		return nil
+	}
+
+	// form initial query
+	query := "INSERT INTO tag_questions (question_id, tag_id) VALUES"
+
+	// add values to the query
+	for i, item := range tagArr {
+		if i != len(tagArr)-1 {
+			query += "(" + question.Id + "," + item + "),"
+		} else {
+			query += "(" + question.Id + "," + item + ");"
+		}
+	}
+
+	// had to take a dummy variable cuz my stupid idea would not let me submit the initial query with syntax error!
+	dummyQuery := query
+	_, err := db.Exec(dummyQuery)
+	return err
+}
+
+func (question *Question) GetTags() ([]string, error) {
+	r, err := db.Query(`SELECT tag_id FROM tag_questions WHERE question_id = $1`, question.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	// scan the tags
+	var qTags []string
+	for r.Next() {
+		var tag string
+		err = r.Scan(&tag)
+		if err != nil {
+			return nil, errors.New("error fetching the answers")
+		}
+
+		qTags = append(qTags, tag)
+	}
+
+	return qTags, nil
 }
